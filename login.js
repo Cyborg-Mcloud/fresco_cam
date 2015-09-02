009191635598af22da9e56c32553c8b4f37b7f44
Cyborg-Mcloud <uabashidze@gmail.com>
2015-09-02 06:34:26 +0400

    g
    f

--- login.js
+++ login.js
@@ -33,3 +33,29 @@ function fblogin()
 	{
 	change_slide(3);
 	}
+
+
+function save_prof()
+	{
+	uname=document.getElementById("prof_uname").value;
+	email=document.getElementById("prof_email").value;
+	persid=document.getElementById("prof_persid").value;
+	var mg=document.getElementById("prof_myfemale");
+	gender=1;
+	if (mg.checked==true)
+		{gender=1;}	
+	else
+		{gender=2;}
+
+	if (uname=="" || email=="" || persid=="")
+		{
+		alert("გთხოვთ შეიყვანოთ სახელი, ელ.ფოსტა და პირადი ნომერი");
+		}
+	else
+		{
+		url='http://design.ge/fresco/save_prof.php?myid='+myid+'&uname='+uname+'&email='+email+'&persid='+persid+'&gender='+gender;
+		gamehttp.open('GET',url,true);
+		gamehttp.send();
+
+		}
+	}
