dbe594c37c46d9739686577eede0d9c09e11da14
Cyborg-Mcloud <uabashidze@gmail.com>
2015-09-29 06:40:44 +0400

    re
    re

--- func.js
+++ func.js
@@ -204,7 +204,7 @@ function chgarleft()
 	if (selgarstyle>1)
 		{
 		selgarstyle-=1;
-		focussel("garstyle_sel",garstylewidth, selsgartyle);
+		focussel("garstyle_sel",garstylewidth, selgartyle);
 		}
 	}
 
